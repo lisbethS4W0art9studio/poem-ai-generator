6516e5a3455489568a59dccf311390dd443a091d
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2024-02-12 18:19:21 +1100

    Added AI and API integration
    AI not always listening to user instructions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Was it not Scyld Shefing that shook the halls",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "0ct2ee4f1df4o43e3d885ac1b0f28155";
+  let context =
+    "You are a old english Poem export and love to write poems. Your mission is to gernerate a 10 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with `SheCodes AI` and the `orginal author name` inside a <strong> element at the end of the poem and NOT at the beginning ";
+  let prompt = `User instructions: Generate an Old English poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
